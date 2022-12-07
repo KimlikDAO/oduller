538947e8e802490e7469548dd8369f425b8c2298
MuhammetCoskun <84336680+MuhammetCoskun@users.noreply.github.com>
2022-12-08 00:03:24 +0300

    🎁 Ödüller sayfası UI geliştir (#2)

--- oduller/sayfa.js
+++ oduller/sayfa.js
@@ -5,44 +5,61 @@
 import Cüzdan from '/birim/cüzdan/birim';
 import dom from '/lib/util/dom';
 import TCKT from '/lib/ethereum/TCKT';
-import SDK from '/sdk/src';
+import kimlikdao from '/sdk/client/kimlikdao';
+import ConfettiGenerator from './confetti';
 
 /** @const {Element} */
 const cüzdanBağlaDüğmesi = dom.adla("ods0b");
+cüzdanBağlaDüğmesi.onclick = Cüzdan.bağla;
 
-cüzdanBağlaDüğmesi.onclick = Cüzdan.bağla();
-Cüzdan.bağlanınca((adres) => {
+const validator = new kimlikdao.Validator('http://localhost:8787/validate')
+
+const hataMesajlari = dom.TR
+  ? [
+    "",
+    "Kampanyaya daha önce katılmışsınız 👍",
+    "Bilgileriniz Hatalı."
+  ] :
+  [
+    "",
+    "You have participated in the campaign before 👍",
+    "Your information is incorrect."
+  ];
+
+Cüzdan.bağlanınca(() => {
   dom.adla("ods0c").classList.add("done");
   dom.butonDurdur(cüzdanBağlaDüğmesi);
   cüzdanBağlaDüğmesi.classList.remove("act");
   cüzdanBağlaDüğmesi.innerText = dom.TR ? "Cüzdan bağlandı ✓" : "Wallet connected ✓";
-  tcktKontrolEt(adres, Cüzdan.ağ());
+  kimlikdao.hasTckt().then((present) => {
+    if (true) {
+      dom.adlaGöster('ods1ac')
+      dom.adla('ods1ab').onclick = bilgileriKontrolEt;
+    } else dom.adlaGöster('ods1bc');
+  })
 });
 
-/**
- * @param {string} adres
- * @param {string} adres
- */
-const tcktKontrolEt = (adres, chainId) => {
-  if (SDK.doesOwnTCKT(adres, chainId)) {
-    dom.adlaGöster("ods1ac");
-    dom.adla("ods1ab").onclick = () => { // TCKT humanId ve contactInfo 
-      bilgileriKontrolEt();
-    }
-  } else {
-    dom.adlaGöster("ods1bc");
-  }
+const bilgileriKontrolEt = () => {
+  kimlikdao.validateTckt(['contactInfo', 'humanID'], validator, false)
+    .then(res => res.json())
+    .then(res => {
+      dom.adla("ods1ac").classList.add("done");
+      dom.butonDurdur(dom.adla("ods1ab"));
+      dom.adla("ods1ab").classList.remove("act");
+      dom.adla('ods1ab').innerText = dom.TR ? "Bilgilerinizi aldık ✓" : "We got your info ✓";
+      if (res.success) {
+        dom.adlaGöster('ods2ac');
+        dom.adlaGöster('odconfetti');
+        setTimeout(() => dom.adlaGizle('odconfetti'), 3000);
+        dom.adla('odtx').innerHTML =
+          `<a href="https://${Cüzdan.AğBilgileri[Cüzdan.ağ()][0]}/tx/${res.txHash}" target="_blank" rel="noopener noreferrer">${res.txHash}</a>`
+      } else {
+        dom.adlaGöster('ods2bc');
+        dom.adla('ods2bt').innerText = hataMesajlari[res.error];
+      }
+    })
 }
 
-const bilgileriKontrolEt = () => {
-  dom.adla("ods1ac").classList.add("done");
-  dom.butonDurdur(dom.adla("ods1ab"));
-  dom.adla("ods1ab").classList.remove("act");
-  dom.adla("ods1ab").innerText = dom.TR ? "Bilgileriniz alındı ✓" : "We got your info ✓";
-  if (true) {   // Kampanyaya kaydı kontrol edilecek
-    dom.adlaGöster("ods2ac");
-    dom.adla("odtx").innerText = "Transaction"; //tx gelecek
-  } else {
-    dom.adlaGöster("ods2bc");
-  }
-}
+let confettiSettings = { target: 'odconfetti' };
+let confetti = new ConfettiGenerator(confettiSettings);
+confetti.render();
